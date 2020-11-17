ab70a831501ca00055d8d00524adb61bd878ea20
Administrator <julija.veigure@gmail.com>
2020-11-17 14:03:59 +0200

    Homework nr1, don`t show correct decimals. Nr. 3 correct.

--- src/test/java/HomeWork1.java
+++ src/test/java/HomeWork1.java
@@ -1,9 +1,15 @@
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
+import static java.lang.Math.round;
 import static java.lang.Math.sqrt;
 
 public class HomeWork1 {
     @Test
+
     public void homeWorks() {
 
         System.out.println("\nHOMEWORK Nr. 1" +
@@ -12,8 +18,16 @@ public class HomeWork1 {
 
         System.out.println("Kopēja mājas cena 39000.00 eur");
         System.out.println("Procenta summa 8%");
-        double q = 39000.00 * 1.08;
-        System.out.println("Atmaksa kopā ar procentiem " + q + " eur");
+        double res = payOut(39000, 1.08);
+        System.out.println("Atmaksa kopā ar procentiem " + res + " eur." );
+
+        BigDecimal twoDec = new BigDecimal(res).setScale(2, RoundingMode.HALF_UP);
+        double resFormat = twoDec.doubleValue();
+        System.out.println("Atmaksa kopā ar procentiem " + resFormat + " eur." );
+
+        System.out.println("Atmaksa kopā ar procentiem " );
+        System.out.printf("%.2f", resFormat);
+        System.out.println(" eur.");
 
         System.out.println("\nHOMEWORK Nr. 2, version nr.1"); {
             // Этот метод Copy -> Paste с нета :)//
@@ -73,13 +87,25 @@ public class HomeWork1 {
         {
             // Mājas = x1, y1;
             // Skola = x2, y2;
-            double result = attalums(0.9919, 0.4275, 0.9919, 0.4280);
-            System.out.println("Attālums no mājām līdz ielas galam " + result + " km.");
+            double result = dist (0.9919, 0.4275, 0.9919, 0.4280);
+            BigDecimal threeDecimal = new BigDecimal(result).setScale(3, RoundingMode.HALF_UP);
+            double threeNumbers = threeDecimal.doubleValue();
+            System.out.println("Attālums no mājām līdz ielas galam " + threeNumbers + " km.");
+            String resultFormat = new DecimalFormat("##.###").format(result);
+            System.out.println("Attālums no mājām līdz ielas galam " + resultFormat + " km.");
+            System.out.println("Attālums no mājām līdz ielas galam: ");
+            System.out.printf("%.3f",result);
+            System.out.println(" km.");
+
         }
     }
-        private double attalums(double x1, double x2, double y1, double y2) {
+        private double dist (double x1, double x2, double y1, double y2) {
         double ab = sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
         return ab;
+    }
+    private double payOut (double z, double s) {
+        double zs = z*s;
+        return  zs;
     }
         }
 
