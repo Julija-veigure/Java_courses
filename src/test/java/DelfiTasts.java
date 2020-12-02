94e40a52e9623df6932f8a8ed0915b9a625e96f2
Administrator <julija.veigure@gmail.com>
2020-12-02 22:11:09 +0200

    Homework

--- src/test/java/DelfiTasts.java
+++ src/test/java/DelfiTasts.java
@@ -1,14 +1,27 @@
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class DelfiTasts {
+    private final By ACCEPT_COOKIE_BTN = By.xpath(".//button[@mode = 'primary']");
+
+
     @Test
 
-    public void firstDelfiTest () {
+    public void firstTvnetTest () {
         System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
         driver.get("http://delfi.lv");
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(ACCEPT_COOKIE_BTN));
+        WebElement acceptBtn = driver.findElement(ACCEPT_COOKIE_BTN);
+        acceptBtn.click();
 
 
     }
