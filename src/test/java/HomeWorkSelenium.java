615ab5a5580ac281d351000b902af021a1d77679
Administrator <julija.veigure@gmail.com>
2020-12-03 18:44:22 +0200

    Papildinats majasdarbs

--- src/test/java/HomeWorkSelenium.java
+++ src/test/java/HomeWorkSelenium.java
@@ -2,21 +2,18 @@
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.util.ArrayList;
 
 
 public class HomeWorkSelenium   {
     private final By ACCEPT_COOKIE_BTN = By.xpath(".//button[@class = 'notification__button']");
     private final By NEWS = By.xpath(".//span[@itemprop = 'headline name']");
-    private final By COMMENTS_NEWS = By.xpath(".//a[@class = 'article-share__item article-share__item--comments article-share__item-with-count']");
+    private final By COMMENTS_NEWS = By.xpath(".//a[contains(@class,'-share__item article-share__item--comments')]");
     // .//span[@class = 'article-share__item--count']
-    // .//a[@class = 'article-share__item article-share__item--comments article-share__item-with-count']
-    private final By ALL_NEWS = By.xpath(".//span[@itemprop = 'headline name']");
+    // .//a[contains(@class, 'article-share__item article-share__item--comments']
+      private final By ALL_NEWS = By.cssSelector(".list-article__headline");
 
 
-    @Test
+     @Test
     public void homeworkTvnet()  {
         //"Homework Nr.1
         // Open web www.tvnet.lv; Click accept bt`n; Open the news; Open new comments;
@@ -29,15 +26,33 @@ public class HomeWorkSelenium   {
         WebElement acceptBtn = driver.findElement(ACCEPT_COOKIE_BTN);
         acceptBtn.click();
 
+        String titleFromWeb = driver.getTitle();
+        System.out.println("Page title: " + titleFromWeb);
+
         WebElement openNews = driver.findElement(NEWS);
         openNews.click();
+
+        String titleNews = driver.getTitle();
+        String titleText = driver.findElement(NEWS).getText();
+        System.out.println("getTitle: " + titleNews);
+        System.out.println("getText: " + titleText);
+
+        String title = "Covid-19: reģistrēts līdz šim lielākais saslimšanas gadījumu " +
+                "skaits - 930 - Latvijā - Ziņas - TVNET";
+
+         System.out.println("From List: " + title);
+        boolean titleRes = titleNews.equals(title);
+        boolean titleResultat = titleNews.contains(title);
+         System.out.println("The title is spelled correctly: " + titleRes);
+
+
+
 
         WebElement listOfComments = driver.findElement(COMMENTS_NEWS);
         listOfComments.click();
 
 
-
-        driver.quit();
+       // driver.quit();
 
         System.out.println("Test DONE!");
 
