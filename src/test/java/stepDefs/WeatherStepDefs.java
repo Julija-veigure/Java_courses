43bae3c3095f831de695b1f582245af839d304aa
Julija Veigure <julija.veigure@gmail.com>
2021-02-21 17:02:17 +0200

    Before changes

--- src/test/java/stepDefs/WeatherStepDefs.java
+++ src/test/java/stepDefs/WeatherStepDefs.java
@@ -1,25 +1,112 @@
 package stepDefs;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import model.WeatherResponse;
+import org.junit.jupiter.api.Assertions;
+import requesters.WeatherRequester;
+
+import javax.sound.midi.Soundbank;
+import java.util.Map;
 
 public class WeatherStepDefs {
+
+    private int cityId;
+    private WeatherResponse response;
+
+
     @Given("city id: {int}")
     public void set_city_id(int cityId) {
-
+        this.cityId = cityId;
     }
+
     @When("we are requesting weather data")
-    public void request_weather() {
-
+    public void request_weather() throws JsonProcessingException {
+        WeatherRequester requester = new WeatherRequester();
+        response = requester.getWeather(cityId);
     }
+
     @Then("lon is {double}")
-    public void check_lon(double lon){
+    public void check_lon(double lon) {
+        Assertions.assertEquals(lon, response.getCoord().getLon(), "Wrong lon");
 
     }
-    @And("lat is {double}")
+
+    @Then("lat is {double}")
     public void check_lat(double lat) {
+        Assertions.assertEquals(lat, response.getCoord().getLat(), "Wrong lat");
 
     }
+
+    @Then("weather is:")
+    public void weathers_data(Map<String, String> weather) {
+
+        Assertions.assertEquals(Integer.valueOf(weather.get("id")), response.getWeathers().get(0).getId(), "Wrong weathers id");
+        Assertions.assertEquals(weather.get("main"), response.getWeathers().get(0).getMain(), "Wrong weathers main");
+        Assertions.assertEquals(weather.get("description"), response.getWeathers().get(0).getDescription(), "Wrong weathers description");
+        Assertions.assertEquals(weather.get("icon"), response.getWeathers().get(0).getIcon(), "Wrong weathers icon");
+    }
+
+    @Then("base is {string}")
+    public void weather_station(String base) {
+        Assertions.assertEquals(base, response.getBase(), "Wrong base");
+    }
+
+    @Then("data from weather station main:")
+    public void data_from_weather_station(Map<String, String> main) {
+        Assertions.assertEquals(Double.valueOf(main.get("temp")), response.getMain().getTemp(), "Wrong main temp");
+        Assertions.assertEquals(Double.valueOf(main.get("pressure")), response.getMain().getPressure(), "Wrong main pressure");
+        Assertions.assertEquals(Integer.valueOf(main.get("humidity")), response.getMain().getHumidity(), "Wrong main humidity");
+        Assertions.assertEquals(Double.valueOf(main.get("temp_min")), response.getMain().getTempMin(), "Wrong main tempMin");
+        Assertions.assertEquals(Double.valueOf(main.get("temp_max")), response.getMain().getTempMax(), "Wrong main tempMax");
+    }
+
+    @Then("visibility is {int}")
+    public void visibility_is(int visibility) {
+        Assertions.assertEquals(visibility, response.getVisibility(), "Wrong visibility");
+    }
+
+    @And("wind speed is {double} and deg is {int}")
+    public void wind_speed_and_deg(double speed, int deg) {
+        Assertions.assertEquals(speed, response.getWind().getSpeed(), "Wrong wind speed");
+        Assertions.assertEquals(deg, response.getWind().getDeg(), "Wrong wind deg");
+
+    }
+
+    @And("clouds is {int}")
+    public void clouds_is(int clouds) {
+        Assertions.assertEquals(clouds, response.getClouds().getAll(), "Wrong clouds");
+    }
+
+    @And("dt is {int}")
+    public void dt_is(int dt) {
+        Assertions.assertEquals(dt, response.getDt(), "Wrong dt");
+    }
+
+    @And("sys:")
+    public void sys(Map<String, String> sys) {
+        Assertions.assertEquals(Integer.valueOf(sys.get("type")), response.getSys().getType(), "Wrong sys type");
+        Assertions.assertEquals(Integer.valueOf(sys.get("id")), response.getSys().getId(), "Wrong sys id");
+        Assertions.assertEquals(Double.valueOf(sys.get("message")), response.getSys().getMessage(), "Wrong sys message");
+        Assertions.assertEquals(sys.get("country"), response.getSys().getCountry(), "Wrong sys country");
+        Assertions.assertEquals(Integer.valueOf(sys.get("sunrise")), response.getSys().getSunrise(), "Wrong sys sunrise");
+        Assertions.assertEquals(Integer.valueOf(sys.get("sunset")), response.getSys().getSunset(), "Wrong sys sunset");
+    }
+
+
+    @And("name is {string}")
+    public void name_is(String name) {
+        Assertions.assertEquals(name, response.getName(), "Wrong name");
+    }
+
+    @And("cod {int}")
+    public void cod(int cod) {
+        Assertions.assertEquals(cod, response.getCod(), "Wrong cod");
+    }
+
+
 }
--- src/test/resources/weather.feature
+++ src/test/resources/weather.feature
@@ -1,6 +1,43 @@
 Feature: Weather API
+
   Scenario: Testing City coordinates
     Given city id: 524901
     When we are requesting weather data
     Then lon is 145.77
     And lat is -16.92
+
+    And weather is:
+      | id          | 802              |
+      | main        | Clouds           |
+      | description | scattered clouds |
+      | icon        | 03n              |
+
+    And base is "stations"
+
+    And data from weather station main:
+      | temp        | 300.15 |
+      | pressure    | 1007   |
+      | humidity    | 74     |
+      | temp_min    | 300.15 |
+      | temp_max    | 300.15 |
+
+
+    And visibility is 10000
+    And wind speed is 3.6 and deg is 160
+    And clouds is 40
+    And dt is 1485790200
+
+    And sys:
+      | type    | 1          |
+      | id      | 8166       |
+      | message | 0.2064     |
+      | country | AU         |
+      | sunrise | 1485720272 |
+      | sunset  | 1485766550 |
+
+    And name is "Cairns"
+    And cod 200
+
+
+
+
