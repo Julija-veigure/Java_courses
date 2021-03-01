a43bf28710b2e575ca4cef65a31c03c387fbf8bd
Julija Veigure <julija.veigure@gmail.com>
2021-03-01 19:59:01 +0200

    changes in branch

--- src/test/java/GitTest.java
+++ src/test/java/GitTest.java
@@ -1,5 +1,7 @@
 public class GitTest {
     public static void main (String[] args){
        String string = "String";
+       String bla = "Stringaaaa";
+        System.out.println("print");
     }
     }
