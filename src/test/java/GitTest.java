1b4131a4e2567d99d8b2597531a6f9fdbbc49ed7
Julija Veigure <73426457+Julija-veigure@users.noreply.github.com>
2021-03-19 22:54:54 +0200

    Delete GitTest.java

--- src/test/java/GitTest.java
+++
@@ -1,7 +0,0 @@
-public class GitTest {
-    public static void main (String[] args){
-       String string = "String";
-       String bla = "Stringaaaa";
-        System.out.println("print");
-    }
-    }
